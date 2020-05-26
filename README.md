c660429aee2b02b5d3d8e7ac9587b5ffec92c4c0
butkej <butkej@gmail.com>
2020-05-26 14:02:28 +0200

    fully working tool. version 0.2

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
 # Simple Image Clustering
-Simple (or more fittingly stupid) image clustering [*sic*] is a very barebones command line tool written in Python3.  
+Simple (or more fittingly: stupid) image clustering [*sic*] is a very barebones command line tool written in Python 3.  
 ### Dependencies
-*sic* requires scikit-learn to perform clustering and matplotlib to visualize and manipulate files.
+*sic* requires opencv2 to perform clustering and manipulate files.
 
 ### Usage
 
+`$(python) sic.py [-h] -i INFILE [-c NUM_CLUSTERS] [-r NUM_RUNS] [-e EXTENSION] [--version]`
+
+simple image clustering based on k-means clustering
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -i INFILE, --infile INFILE
+                        you must provide an input image to cluster it
+  -c NUM_CLUSTERS, --clusters NUM_CLUSTERS
+                        choose the number of clusters to produce (default is 5) [integer]
+  -r NUM_RUNS, --runs NUM_RUNS
+                        choose the number of clustering runs to attempt (default is 10) [integer]
+  -e EXTENSION, --extension EXTENSION
+                        choose the filename extension of the saved clustered
+                        image (default is png) [string]
+  --version             show program's version number and exit
--- sic.py
+++ sic.py
@@ -6,9 +6,10 @@ import cv2
 def parse_args():
     parser = argparse.ArgumentParser(prog='sic', description='simple image clustering based on k-means clustering')
 
-    parser.add_argument('-i', '--infile', type=argparse.FileType('r'), required=True, help='you must provide an input image to cluster it')
-    parser.add_argument('-c','--clusters', type=int, default=5, dest='num_clusters', help='choose the number of clusters to produce [integer]')
-    parser.add_argument('-r', '--runs', type=int, default=10, dest='num_runs', help='choose the number of clustering runs to attempt [integer]')  )
+    parser.add_argument('-i', '--infile', type=str, required=True, help='you must provide an input image to cluster it')
+    parser.add_argument('-c','--clusters', type=int, default=5, dest='num_clusters', help='choose the number of clusters to produce (default is 5) [integer]')
+    parser.add_argument('-r', '--runs', type=int, default=10, dest='num_runs', help='choose the number of clustering runs to attempt (default is 10) [integer]')
+    parser.add_argument('-e', '--extension', type=str, default='png', dest='extension', help='choose the filename extension of the saved clustered image (default is png) [string]')
     parser.add_argument('--version', action='version', version='%(prog)s 0.2')
 
     args = parser.parse_args()
@@ -22,8 +23,9 @@ if __name__ == "__main__":
 
     # load image
     print('Loading input image...')
-    img = cv2.imread(args.infile)
-    img = cv2.cvtColor(original_image,cv2.COLOR_BGR2RGB)
+    print(args.infile)
+    img = cv2.imread(str(args.infile))
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 
     # preprocessing
     img_shape = img.shape
@@ -35,14 +37,18 @@ if __name__ == "__main__":
     flags = cv2.KMEANS_PP_CENTERS
 
     print('Starting clustering...')
-    compactness, labels, centers = cv2.kmeans(img, args.num_clusters, args.num_runs, criteria, flags) 
+    compactness, labels, centers = cv2.kmeans(img, K=args.num_clusters, bestLabels=None, attempts=args.num_runs, criteria=criteria, flags=flags)
     
-    center = np.uint8(center)
-    result = center[labels.flatten()]
+    centers = np.uint8(centers)
+    result = centers[labels.flatten()]
     result_img = result.reshape((img_shape))
 
     # save clustered image
     print('Saving clustered image to ...')
-
+    savepath = str(args.infile).split('.')
+    savepath = savepath[0] + '_' + str(args.num_clusters) + '_CLUSTERS.' + str(args.extension)
+    print(savepath)
+    result_img = cv2.cvtColor(result_img, cv2.COLOR_RGB2BGR)
+    cv2.imwrite(savepath, result_img)
     print('Done!')
 
