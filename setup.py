31219a6779ef3227043f20bfb4990ef30551b005
butkej <butkej@gmail.com>
2020-05-27 10:52:58 +0200

    getting setup right...

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 setup(
         name = 'sic',
         version = '0.2',
         description = 'simple image clustering',
-        packages = ['sic'],
+        package_dir = {'': 'sic'},
+        packages = find_packages(where='sic'),
         install_requires =['numpy', 'opencv-python'],
-        entry_points = {'console_scripts': ['sic = sic.sic:main']}
+        entry_points = {'console_scripts': ['sic=sic:main',],}
         )
