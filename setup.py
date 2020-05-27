a6a1a572a09812ba061373e5df5cba47c9193b46
butkej <butkej@gmail.com>
2020-05-27 11:25:06 +0200

    setup and install works and enables script calling from $PATH

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 setup(
         name = 'sic',
         version = '0.2',
+        author = 'Joshua Butke',
+        author_email= 'butkej@gmail.com',
         description = 'simple image clustering',
-        package_dir = {'': 'sic'},
-        packages = find_packages(where='sic'),
+        #package_dir = {'': 'sic'},
+        packages = ['sic'],
         install_requires =['numpy', 'opencv-python'],
-        entry_points = {'console_scripts': ['sic=sic:main',],}
+        #entry_points = {'console_scripts': ['sic=sic.sic:main'],},
+        scripts=['bin/sic']
         )
